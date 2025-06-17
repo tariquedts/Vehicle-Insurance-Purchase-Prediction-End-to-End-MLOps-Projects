b14eefb48e8ac6015557d7da1cddf8d3e6cd8147
Tarique Anwar <166940625+tariquedts@users.noreply.github.com>
2025-06-17 22:11:29 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="src",
     version="0.0.1",
-    author="Vikash Das",
-    author_email="vikashdas770@gmail.com",
+    author="Tarique Anwar",
+    author_email="tarique.dhs@gmail.com",
     packages=find_packages()
 )
